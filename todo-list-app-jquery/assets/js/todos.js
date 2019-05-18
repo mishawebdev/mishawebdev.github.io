ae8e4b2dfb8b77d35409d1e7bc253667b0805f40
Misha <50199653+mishawebdev@users.noreply.github.com>
2019-05-18 17:15:46 +0300

    add todo app
    add todo app

---
+++ todo-list-app-jquery/assets/js/todos.js
@@ -0,0 +1,28 @@
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function () {
+  $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function (event) {
+  $(this).parent().fadeOut(500, function () {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+// Create a new Todo
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    // grabbing new Todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    // create a new li and add it to ul
+    $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")
+  }
+});
+
+// Toggle the input
+$(".fa-pen-alt").click(function(){
+  $("input[type='text']").fadeToggle();
+});
